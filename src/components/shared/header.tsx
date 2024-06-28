537995bf284ef9d3e5bb245326e808996f3da1c8
kimia <farshadniakimia@gmail.com>
2024-06-28 19:37:28 +0330

    inc z index in header

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -10,7 +10,7 @@ export const Header = () => {
 	const pathname = usePathname();
 
 	return (
-		<header className='shadow-lg bg-transparent backdrop-blur-xl dark:text-white w-full fixed z-20'>
+		<header className='shadow-lg bg-transparent backdrop-blur-xl dark:text-white w-full fixed z-50'>
 			<div className='container mx-auto'>
 				<div className='w-full py-5 flex justify-between items-center'>
 					<div className='pl-5 md:pl-0 flex items-center gap-4'>
