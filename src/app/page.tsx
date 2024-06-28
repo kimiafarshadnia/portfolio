b3725aa1b4aa159ec0cef7fd2311f2da96b7bfa5
kimia <farshadniakimia@gmail.com>
2024-06-28 19:28:32 +0330

    remove overflow hidden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-20 md:gap-36">
       <div className='absolute flex justify-center items-center top-[5rem] right-[-25rem]'>
-        <div className='w-[500px] h-[500px] md:w-[700px] md:h-[700px] rounded-full bg-shadow1 opacity-50 blur-2xl relative overflow-x-hidden animation1'></div>
+        <div className='w-[500px] h-[500px] md:w-[700px] md:h-[700px] rounded-full bg-shadow1 opacity-50 blur-2xl relative animation1'></div>
       </div>
 
       <div className='absolute flex justify-center items-center left-[-15rem]'>
