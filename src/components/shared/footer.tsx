e0dae31999ade3f3b42a5c57f6d704cf432c002b
kimia <farshadniakimia@gmail.com>
2024-06-28 19:34:32 +0330

    footer changed

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -4,11 +4,11 @@ export const Footer = () => {
         <footer className='dark:bg-darkMode dark:text-white px-5 md:px-0'>
             <div className='container mx-auto flex flex-col gap-6'>
                 <div className='bg-gradient-to-r from-[#C6C6C600] via-[#C6C6C6] to-[#C6C6C600] h-px'></div>
-                <div className='w-full pb-6 flex flex-col md:flex-row justify-center items-center md:items-baseline md:justify-between gap-6 md:gap-0'>       
-                        <p>© {(new Date().getFullYear())} All rights reserved</p>
-                        <div>
-                            brand
-                        </div>
+                <div className='w-full pb-6 flex flex-col-reverse md:flex-row justify-center items-center md:items-baseline md:justify-between gap-6 md:gap-0'>
+                    <p>© {(new Date().getFullYear())} All rights reserved</p>
+                    <div>
+                        brand
+                    </div>
                 </div>
             </div>
         </footer>
